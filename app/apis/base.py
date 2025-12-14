4e8aad79338611d3097c61f6dfc1961bc581eb62
Harshit <pawarharshit358@gmail.com>
2025-12-14 22:11:29 +0530

    added logs

--- app/apis/base.py
+++ app/apis/base.py
@@ -1,13 +1,21 @@
-from fastapi import APIRouter, status
-
+import structlog
+from fastapi import APIRouter, status, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from app.db.base import get_db
 from app.apis.response import ResponseEntity
 from app.apis.v1.base import router as router_v1
 
 router = APIRouter(prefix="/api")
 
+logger = structlog.get_logger(__name__)
+
 
 @router.get("/health")
-def health():
+def health(db: Annotated[Session, Depends(get_db)]):
+    logger.info("Health check started")
+    result = db.execute("SELECT 1")
+    logger.info("Health check completed", result=result)
     return ResponseEntity(
         code=status.HTTP_200_OK, message="I am healthy", data={}
     ).to_response()
