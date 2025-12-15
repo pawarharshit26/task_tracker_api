3c5a63830b0f58053f47ed650b666b0737e62319
Harshit-creditas <harshit.pawar@creditas.in>
2025-12-15 12:51:34 +0530

    fixed health check

--- app/apis/base.py
+++ app/apis/base.py
@@ -2,6 +2,7 @@ import structlog
 from fastapi import APIRouter, status, Depends
 from typing import Annotated
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from app.db.base import get_db
 from app.apis.response import ResponseEntity
 from app.apis.v1.base import router as router_v1
@@ -12,10 +13,10 @@ logger = structlog.get_logger(__name__)
 
 
 @router.get("/health")
-def health(db: Annotated[Session, Depends(get_db)]):
+async def health(db: Annotated[Session, Depends(get_db)]):
     logger.info("Health check started")
-    result = db.execute("SELECT 1")
-    logger.info("Health check completed", result=result)
+    result = await db.execute(text("SELECT 1"))
+    logger.info("Health check completed, database connected successfully")
     return ResponseEntity(
         code=status.HTTP_200_OK, message="I am healthy", data={}
     ).to_response()
