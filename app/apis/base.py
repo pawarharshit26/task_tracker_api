3bf2160d69ae49eaf664b00a046c12290d2672a2
Harshit-creditas <harshit.pawar@creditas.in>
2025-12-17 11:29:46 +0530

    added ruff conf

--- app/apis/base.py
+++ app/apis/base.py
@@ -1,11 +1,13 @@
-import structlog
-from fastapi import APIRouter, status, Depends
 from typing import Annotated
-from sqlalchemy.orm import Session
+
+import structlog
+from fastapi import APIRouter, Depends, status
 from sqlalchemy import text
-from app.db.base import get_db
+from sqlalchemy.orm import Session
+
 from app.apis.response import ResponseEntity
 from app.apis.v1.base import router as router_v1
+from app.db.base import get_db
 
 router = APIRouter(prefix="/api")
 
@@ -17,9 +19,7 @@ async def health(db: Annotated[Session, Depends(get_db)]):
     logger.info("Health check started")
     await db.execute(text("SELECT 1"))
     logger.info("Health check completed, database connected successfully")
-    return ResponseEntity(
-        code=status.HTTP_200_OK, message="I am healthy", data={}
-    )
+    return ResponseEntity(code=status.HTTP_200_OK, message="I am healthy", data={})
 
 
 router.include_router(router_v1)
